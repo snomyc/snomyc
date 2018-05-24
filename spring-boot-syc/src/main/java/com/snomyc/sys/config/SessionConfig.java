059ca28e167487072ca1537473fc6cd7c38f4a1b
snomy <snomy@DESKTOP-N66L1KK>
2018-05-24 15:07:51 +0800

    提交

--- spring-boot-syc/src/main/java/com/snomyc/sys/config/SessionConfig.java
+++ spring-boot-syc/src/main/java/com/snomyc/sys/config/SessionConfig.java
@@ -1,9 +1,16 @@
-//package com.snomyc.sys.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 86400*30)
-//public class SessionConfig {
-//	
-//}
+package com.snomyc.sys.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+/**
+ * @author yangcan
+ * 类描述:  maxInactiveIntervalInSeconds: 设置Session失效时间，使用Redis Session之后，原Boot的server.session.timeout属性不再生效
+ * 创建时间:2018年5月24日 下午2:51:10
+
+ */
+@Configuration
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 86400*30)
+public class SessionConfig {
+	
+}
--- spring-boot-syc/src/main/java/com/snomyc/web/HelloController.java
+++ spring-boot-syc/src/main/java/com/snomyc/web/HelloController.java
@@ -1,17 +1,17 @@
 package com.snomyc.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 	
-//	@Autowired
-//    private StringRedisTemplate stringRedisTemplate;
+	@Autowired
+    private StringRedisTemplate stringRedisTemplate;
 	
     @RequestMapping("/hello")
     public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
@@ -19,9 +19,10 @@ public class HelloController {
         return "hello";
     }
 
-//    @RequestMapping(value = "/redis/string", method = RequestMethod.GET)
-//    public void insertString() {
-//        stringRedisTemplate.opsForValue().set("TOKEN-yc", "123456");
-//    }
+    @RequestMapping(value = "/redis")
+    public String insertString() {
+        stringRedisTemplate.opsForValue().set("TOKEN-yc", "123456");
+        return "hello";
+    }
 
 }
