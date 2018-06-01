0f6425747e4f943cd608fec65250531c41e799df
snomy <snomy@qq.com>
2018-06-01 13:52:22 +0800

    提交缓存配置

--- spring-boot-syc/src/main/java/com/snomyc/base/redis/JedisPoolFactory.java
+++ spring-boot-syc/src/main/java/com/snomyc/base/redis/JedisPoolFactory.java
@@ -1,4 +1,4 @@
-package com.snomyc.base.config;
+package com.snomyc.base.redis;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- spring-boot-syc/src/main/java/com/snomyc/base/redis/RedisTemplateConfig.java
+++ spring-boot-syc/src/main/java/com/snomyc/base/redis/RedisTemplateConfig.java
@@ -1,4 +1,4 @@
-package com.snomyc.base.config;
+package com.snomyc.base.redis;
 //package com.snomyc.sys.config;
 //
 //import java.lang.reflect.Method;
