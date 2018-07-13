0dadf4943f0cc669f166894ab59947eefb846438
snomyc <snomyc@qq.com>
2018-07-13 22:01:50 +0800

    提交问题2

--- amazon-sup/src/main/java/com/snomyc/sys/dao/AmazonKeyWordDao.java
+++ amazon-sup/src/main/java/com/snomyc/sys/dao/AmazonKeyWordDao.java
@@ -12,4 +12,5 @@ public interface AmazonKeyWordDao extends JpaRepository<AmazonKeyWord, String> {
 	public List<AmazonKeyWord> findByKeyWordRoot(String keyWordRoot);
 	
 	public int countByKeyWordRootAndKeyWordSecond(String keyWordRoot,String keyWordSecond);
+	
 }
