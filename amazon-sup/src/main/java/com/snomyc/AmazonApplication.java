8f6db07fe8f46ff87468b8d693d1c8cf19e34f30
yangcan <123456>
2019-05-24 17:14:12 +0800

    新增定时任务

--- amazon-sup/src/main/java/com/snomyc/AmazonApplication.java
+++ amazon-sup/src/main/java/com/snomyc/AmazonApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2 //加载swagger
+@EnableScheduling //定时任务
 public class AmazonApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
